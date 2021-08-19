49766a00134158ddca5fff8198d9f4e13d958564
Mr. Anderson Bonfil <andresbonfil@gmail.com>
2021-08-19 17:14:00 -0500

    checkplantilla-?

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InicioController;
 
+
 Route::get('/', InicioController::class)->name('inicio');
 Route::get('registrarse', function () { return view('registrarse'); })->name('registrarse');
 Route::get('recontra', function () { return view('recontra'); })->name('recontra');
